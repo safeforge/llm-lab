46bebfae125401b9bc398220de4c2874093d3d24
Salvatore Ferrucci <salvatore@algonauti.com>
2025-03-30 23:39:46 +0200

    move Example into dedicated crate and extends with Metadata

--- examples/src/lib.rs
+++ examples/src/lib.rs
@@ -1,10 +1,6 @@
 use std::{collections::HashMap, sync::LazyLock};
+use types::Example;
 
-use anyhow::Result;
-
-pub trait Example: Send + Sync {
-    fn main(&self) -> Result<()>;
-}
 
 pub static REGISTER: LazyLock<HashMap<&'static str, Box<dyn Example>>> = LazyLock::new(|| {
     let m: HashMap<&'static str, Box<dyn Example + 'static>> = HashMap::new();
---
+++ types/src/lib.rs
@@ -0,0 +1,11 @@
+use anyhow::Result;
+
+pub trait Example: Send + Sync + Metadata {
+    fn main(&self) -> Result<()>;
+}
+
+pub trait Metadata {
+    fn id(&self) -> String;
+    fn description(&self) -> String;
+    fn page(&self) -> String;
+}
